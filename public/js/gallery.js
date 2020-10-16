f3a6cfe321c19537920232417ad4f470c7adcdd7
Jacob <jpelrah@wpi.edu>
2020-10-16 17:36:36 -0400

    Updated gallery.js

--- public/js/gallery.js
+++ public/js/gallery.js
@@ -11,48 +11,70 @@ console.log(sessionStorage.getItem("friendData"))
 
 async function updateGalleryDisplay() {
 
-    let drawings = await getDrawings()
-    console.log(drawings)
-
     //handle drawings here
+    let drawings = await getDrawings()
     const gallery = document.querySelector('#gallery')
+    let galleryItems = []
 
-    let myGallery = ''
     for (let i = 0; i < drawings.length; i++) {
-        let template = `
-          <div class="col-md-3">
-            <div class="card bg-dark text-center">
-            <div class="card-header">
-            <h4 class="card-text">${drawings[i].title}</h4>
-            </div>
-                <img id="galleryThumb" class="card-img-top" src="${drawings[i].URI}">
-                    <div class="card-img-overlay text-dark">
-                    </div>
-                        <div class="card-footer bg-transparent">
-                        <div class="card-text">${drawings[i].artist}</div>
-                        <button type="button" class="btn btn-light btn-sm">View</button>
-                        </div>
-                        </div>
-            </div>
-        </div>`
 
-        myGallery += template
+        let cardTitle = document.createElement('h4')
+        cardTitle.classList.add('card-text')
+        cardTitle.innerText = drawings[i].title
+
+        let cardHeader = document.createElement('div')
+        cardHeader.classList.add('card-header')
+        cardHeader.appendChild(cardTitle)
+
+        let galleryThumb = document.createElement('img')
+        galleryThumb.setAttribute('id', 'galleryThumb')
+        galleryThumb.classList.add('card-img-top')
+        galleryThumb.src = drawings[i].URI
+
+        let cardArtist = document.createElement('div')
+        cardArtist.classList.add('card-text')
+        cardArtist.innerText = drawings[i].artist
+
+        let viewButton = document.createElement('a')
+        viewButton.setAttribute('href', drawings[i].URI)
+        viewButton.classList.add('btn-light')
+        viewButton.classList.add('btn-sm')
+        viewButton.innerText = 'View'
+
+        let cardFooter = document.createElement('div')
+        cardFooter.classList.add('card-footer')
+        cardFooter.classList.add('bg-transparent')
+        cardFooter.appendChild(cardArtist)
+        cardFooter.appendChild(viewButton)
+
+
+        let card = document.createElement('div')
+        card.classList.add('card')
+        card.classList.add('bg-dark')
+        card.classList.add('text-center')
+        card.appendChild(cardHeader)
+        card.appendChild(galleryThumb)
+        card.appendChild(cardFooter)
+
+        let container = document.createElement('div')
+        container.classList.add('col-md-3')
+        container.appendChild(card)
+        galleryItems.push(container)
     }
-//console.log(myGallery)
-    console.log(myGallery)
-    gallery.innerHTML = myGallery
+    galleryItems.forEach(drawing =>{
+        gallery.appendChild(drawing)
+    })
 }
 
-
-function getDrawings () {
+function getDrawings() {
 
     let artist = sessionStorage.getItem("friendData") // Pull value set by home
 
     // Get friend data by username
     return fetch("/drawings", {
-        method:"POST",
-        body:JSON.stringify({artist: artist}),
-        headers: { "Content-Type": "application/json"}
+        method: "POST",
+        body: JSON.stringify({artist: artist}),
+        headers: {"Content-Type": "application/json"}
     })
         .then(response => response.json())
         .then(json => {
